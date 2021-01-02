774b9f9fe245b2b5be53d1a24bb12649bdb4f9de
mutoni23 <mutoniv5@gmail.com>
2021-01-02 18:13:57 +0200

    added database

--- insta/settings.py
+++ insta/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 DATABASES = {
     'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           
            'NAME': 'ig',
            'USER': 'wecode',
            'PASSWORD': '12345',
