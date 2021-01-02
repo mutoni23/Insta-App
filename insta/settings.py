2597887ba720863a19be1c73afac55fef73ca6a2
mutoni23 <mutoniv5@gmail.com>
2021-01-02 18:04:07 +0200

    added models

--- insta/settings.py
+++ insta/settings.py
@@ -123,5 +123,7 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
